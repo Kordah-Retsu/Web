df9bb660b1a09f712dd3071b4ceaccb441ae8bcb
Kordah Retsu <retsukodjo@gmail.com>
2025-03-24 14:35:02 +0000

    Buttons. Creating and styling a button using the various styles methods

--- project 02/src/App.jsx
+++ project 02/src/App.jsx
@@ -1,9 +1,9 @@
-import Card from './Card'
+import Button from './Button'
 
 function App() {
   return(
     <>
-     <Card />
+     <Button />
     </>
    
   )
---
+++ project 02/src/Button.jsx
@@ -0,0 +1,20 @@
+
+function Button(){
+
+    const styles ={
+            backgroundColor:"rgba(22, 42, 221, 0.5)",
+            color:"rgb(0, 0, 0)",
+            padding:"10px 20px",
+            borderRadius:"5px",
+            border: "none",
+            cursor: "pointer",
+            margin: "10px",
+    }
+
+
+    return(
+        <button style={styles}>Click me</button>
+    )
+}
+
+export default Button
