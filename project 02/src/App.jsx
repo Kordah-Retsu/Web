ef86038ba69d835a86378144b43355b0f489aeb0
Kordah Retsu <retsukodjo@gmail.com>
2025-03-25 08:36:11 +0000

    Props

--- project 02/src/App.jsx
+++ project 02/src/App.jsx
@@ -1,9 +1,15 @@
-import Button from './Button'
+import Student from './Students.jsx'
+
 
 function App() {
   return(
     <>
-     <Button />
+     <Student name="Retsu" age={21} isStudent={false} />
+     <Student name="Kenshin" age= {22} isStudent={true} />
+     <Student name="Sanosuke" age={27} isStudent={false} />
+     <Student name="Kaoru" age={25} isStudent={true} /> 
+     <Student />
+
     </>
    
   )
